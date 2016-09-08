9853109b32d3b9f5278f1dc12f12ae981c0f47e6
smalllamartin <smalllamartin@gmailcom>
2016-09-08 17:52:09 -0400

    appel a candidature

--- app/controllers/Candidatures.java
+++ app/controllers/Candidatures.java
@@ -3,14 +3,20 @@ package controllers;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.candidature;
+import views.html.incubation;
+import views.html.pre_incubation;
 
 
 public class Candidatures extends Controller {
 
     @Transactional
-    public Result read() {
-        return ok(candidature.render());
+    public Result readIncubation() {
+        return ok(incubation.render());
+    }
+
+    @Transactional
+    public Result readPreIncubation() {
+        return ok(pre_incubation.render());
     }
 
 }
